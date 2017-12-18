0fe043d549cbd4c20762d9fce8940fd176371341
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-18 13:58:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,12 +26,7 @@ function makeInt(n){
   return parseInt(n,10);
 
 }
-// function makeInt(n){
-//
-// }
 function preserveDecimal(n){
-
-}
-function preserveDecimal(n){
+  return parseFloat(n);
 
 }
